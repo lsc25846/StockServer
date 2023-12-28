ede69b147b9c73c478a5de1e8f14883279b72499
Lucas <lsc25846@gmail.com>
2023-12-28 16:57:42 +0800

    Enhanced error logging and implemented input validation for IP and port entries

--- StockServer/Form1.cs
+++ StockServer/Form1.cs
@@ -19,15 +19,32 @@ namespace StockServer
             btnStartServer.BackColor = Color.Gray;
         }
         private void btnStartServer_Click(object sender, EventArgs e)
-        {
-            string ip = txtBoxIP.Text;
-            int port = int.Parse(txtBoxPort.Text);
-            dataServer.StartServer(ip, port);
+        {     
+            string server = txtBoxIP.Text;
+            string portText = txtBoxPort.Text;
+            // 检查服Server地址和port是否有效
+            if (!IsValidIPAddress(server))
+            {
+                MessageBox.Show("無效的伺服器IP。", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(portText, out int port) || port < 0 || port > 65535)
+            {
+                MessageBox.Show("無效的port號碼。", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            dataServer.StartServer(server, port);
             // 更改按钮颜色为绿色
             btnStartServer.BackColor = Color.Green;
 
             // 禁用按钮，防止重复点击
             btnStartServer.Enabled = false;
+        }
+        // 验证IP地址格式是否正确
+        private bool IsValidIPAddress(string ipAddress)
+        {
+            return IPAddress.TryParse(ipAddress, out _);
         }
 
         private void SocketServerForm_FormClosing(object sender, FormClosingEventArgs e)
